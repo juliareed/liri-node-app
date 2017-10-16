6b3236352eef0f42ce1a8bae01a15926a53ce4d0
Julia Reed <msjuliareed@gmail.com>
2017-10-16 09:59:33 -0500

    fixed movie-this

--- liri.js
+++ liri.js
@@ -1,11 +1,11 @@
 var keys = require('./keys.js');
-var twitter = require("twitter");
-// var spotify = require("spotify");
-// var request = require("request");
-// var fs = require('fs');
+
+
+var request = require("request");
+var fs = require('fs');
 
 // Capture user input, and inform user of what to type in
-console.log("Enter my-tweets to play with me!");
+console.log("Enter my-tweets, spotify-this-song, movie-this, or do-what-it-says to play with me!");
 // Process[2] choses action, process[3] as search parameter for spotify or movie
 var userCommand = process.argv[2];
 var secondCommand = process.argv[3];
@@ -22,30 +22,26 @@ function nowSwitch(){
 		fetchTweets();
 		break;
 
-		// case 'spotify-this-song':
-		// spotifyMe();
-		// break;
+		case 'spotify-this-song':
+		spotifyMe();
+		break;
 
-		// case 'movie-this':
-		// movieTime();
-		// break;
+		case 'movie-this':
+		movieTime();
+		break;
 
-		// case 'do-what-it-says':
-		// whatItDo();
-		// break;
+		case 'do-what-it-says':
+		whatItDo();
+		break;
 		
 	}
 };
 // Options.
 function fetchTweets(){
 	console.log("Tweets!");
+	var twitter = require("twitter");
 	// New variable for instance of twitter, load keys from imported keys.js
-	var client = new twitter({
-		consumer_key: 'ntvE6aJIQe30PRBrhotzxFtwT',
-		consumer_secret: 'jAIoF1Mwny0i8SyVrinRy3RRYR8fTQyfBhULYbxC6qMb3HMhMv',
-		access_token_key: '919636867406090240-0sSJKyVXdeYO70pUV9x8sfcxzxyscil',
-		access_token_secret: 'ua23P3nac09J73GnIgg3grvKyINDQJADUWrD81gRQHDC1',
-	});
+	var client = new twitter(keys.twitter);
 
 	// Parameters for twitter function
 	var parameters = {
@@ -65,81 +61,83 @@ function fetchTweets(){
 	});
 };// End fetchTweets
 
-// function spotifyMe(){
-// 	console.log("Dance around!");
+function spotifyMe(){
+	console.log("Dance around!");
 
 // 	// Variable for search term, test if defined
+	var spotify = require("node-spotify-api");
 
-// 	var searchTrack;
-// 	if(secondCommand === undefined){
-// 		searchTrack = "The Sign";
-// 	} else {
-// 		searchTrack = secondCommand;
-// 	}
+	var searchTrack = new spotify(keys.spotify);
+	
+	if(secondCommand === undefined){
+		searchTrack = "The Sign";
+	} else {
+		searchTrack = secondCommand;
+	}
 // 	// Launch spotify search
-// 	spotify.search({type:'track', query:searchTrack}, function(err,data){
-// 	    if(err){
-// 	        console.log('Error occurred: ' + err);
-// 	        return;
-// 	    } else {
-// 	        // Tried searching for release year! Spotify doesn't return this!
-// 	  		console.log("Artist: " + data.tracks.items[0].artists[0].name);
-// 	        console.log("Song: " + data.tracks.items[0].name);
-// 	        console.log("Album: " + data.tracks.items[0].album.name);
-// 	        console.log("Preview song: " + data.tracks.items[0].preview_url);
-// 	    }
-// 	});
-// };// End spotifyMe
+	searchTrack.search({type:'track', query:searchTrack}, function(err, data){
+	    if(err){
+	        console.log('Error occurred: ' + err);
+	        return;
+	    } else {
+	        // Log results
+	  		console.log("Artist: " + data.tracks.items[0].artists[0].name);
+	        console.log("Song: " + data.tracks.items[0].name);
+	        console.log("Album: " + data.tracks.items[0].album.name);
+	        console.log("Preview song: " + data.tracks.items[0].preview_url);
+	    }
+	});
+}; // End spotifyMe
 
-// function movieTime(){
-// 	console.log("Watch this!");
+function movieTime(){
+	console.log("Watch this!");
 
 // 	// Same as above, test if search term entered
-// 	var searchMovie;
-// 	if(secondCommand === undefined){
-// 		searchMovie = "Mr. Nobody";
-// 	} else {
-// 		searchMovie = secondCommand;
-// 	};
+	var searchMovie;
+	if(secondCommand === undefined){
+		searchMovie = "Mr. Nobody";
+	} else {
+		searchMovie = secondCommand;
+	};
 
-// 	var url = 'http://www.omdbapi.com/?t=' + searchMovie +'&y=&plot=long&tomatoes=true&r=json';
-// 	request(url, function(error, response, body){
-// 		if(!error && response.statusCode == 200){
-// 			console.log("Title: " + JSON.parse(body)["Title"]);
-// 			console.log("Year: " + JSON.parse(body)["Year"]);
-// 			console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"]);
-// 			console.log("Country: " + JSON.parse(body)["Country"]);
-// 			console.log("Language: " + JSON.parse(body)["Language"]);
-// 			console.log("Plot: " + JSON.parse(body)["Plot"]);
-// 			console.log("Actors: " + JSON.parse(body)["Actors"]);
-// 			console.log("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
-// 			console.log("Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
-// 		}
-// 	});
-// };// End movieTime
+	var url = 'http://www.omdbapi.com/?t=' + searchMovie +'&y=&plot=short&apikey=40e9cece';
+	request(url, function(error, response, body){
+		if(!error && response.statusCode == 200){
+			console.log("Title: " + JSON.parse(body)["Title"]);
+			console.log("Year: " + JSON.parse(body)["Year"]);
+			console.log("IMDB Rating: " + JSON.parse(body)["imdbRating"]);
+			console.log("Country: " + JSON.parse(body)["Country"]);
+			console.log("Language: " + JSON.parse(body)["Language"]);
+			console.log("Plot: " + JSON.parse(body)["Plot"]);
+			console.log("Actors: " + JSON.parse(body)["Actors"]);
+			console.log("Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]);
+			console.log("Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]);
+		}
+	});
+}; // End movieTime
 
-// function whatItDo(){
-// 	console.log("Looking at random.txt now");
-// 	fs.readFile("random.txt", "utf8", function(error, data) {
-// 		if(error){
-// 			console.log(error);
-// 		} else {
+function whatItDo(){
+	console.log("Looking at random.txt now");
+	fs.readFile("random.txt", "utf8", function(error, data) {
+		if(error){
+			console.log(error);
+		} else {
 
 //      	// Split data, declare variables
-//      	var dataArr = data.split(',');
-//      	userCommand = dataArr[0];
-//      	secondCommand = dataArr[1];
+     	var dataArr = data.split(',');
+     	userCommand = dataArr[0];
+     	secondCommand = dataArr[1];
 //         // If multi-word search term, add
-//         for(i = 2; i < dataArr.length; i++){
-//         	secondCommand = secondCommand + "+" + dataArr[i];
-//         };
+        for(i = 2; i < dataArr.length; i++){
+        	secondCommand = secondCommand + "+" + dataArr[i];
+        };
 //         // Run action
-//         nowSwitch();
+        nowSwitch();
 
-//     	};// End else
+    	}; // End else
 
-//     });// End readfile
+    }); // End readfile
 
-// };// End whatItDo
+}; // End whatItDo
 
 nowSwitch();
